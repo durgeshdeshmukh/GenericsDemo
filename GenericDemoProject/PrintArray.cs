a44bb4aba2158ffe5e3b32f380ea59cfffd9c2d3
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-17 22:56:25 -0800

    Add project files.

---
+++ GenericDemoProject/PrintArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class PrintArray<T>
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (T element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        //public static void toPrint(int[] inputArray)
+        //{
+        //    foreach (var element in inputArray)
+        //    {
+        //        Cons
+    }
+}
---
+++ GenericDemoProject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //public static void toPrint(int[] inputArray)
+            //{
+            //    foreach (var element in inputArray)
+            //    {
+            //        Cons
+        }
+    }
+}
